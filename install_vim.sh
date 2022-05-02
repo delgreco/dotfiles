c0179e2ff1aaafc78aff344124790f1c0ae03a8c
Marcus Del Greco <marcus@mindmined.com>
2022-05-02 21:34:54 +0000

    Install vim in ~/.local instead of /usr/local/bin.

--- install_vim.sh
+++ install_vim.sh
@@ -5,13 +5,15 @@
 cd $HOME
 
 # try to determine which kind of system we are on
-YUM_CMD=$(which yum)
-APT_GET_CMD=$(which apt-get)
+YUM_CMD=$(which yum 2> /dev/null)
+APT_GET_CMD=$(which apt-get 2> /dev/null)
 
 if [[ ! -z $YUM_CMD ]]; then
-    sudo yum install gcc git ncurses-devel
+    #sudo yum install gcc git ncurses-devel
+    echo "yum packages required: gcc git ncurses-devel"
 elif [[ ! -z $APT_GET_CMD ]]; then
-    sudo apt-get install build-essential libncurses-dev git
+    #sudo apt-get install build-essential libncurses-dev git
+    echo "apt-get packages required: build-essential libncurses-dev git"
 else
     echo "Neither yum nor apt-get are present on this system; dependencies may need to be installed manually"
     exit 1;
@@ -21,6 +23,7 @@ git clone https://github.com/vim/vim.git
 
 cd vim/src
 
-sudo make
+#sudo make
+#sudo make install
 
-sudo make install
+./configure --prefix=$HOME/.local && make && make install
